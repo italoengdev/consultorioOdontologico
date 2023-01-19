98368716c488cc67a2a6088d932f0025fa869480
italoengdev <italo_saraiva182@hotmail.com>
2023-01-19 00:34:59 -0300

    quemSomos

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,61 +1,16 @@
 import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
-import Github from './Github'
-import Techstack from './Techstack'
-import Aboutcard from './AboutCard'
-import me4 from '../../Assets/me4.jpeg'
-import game from '../../Assets/TW3_Wild_Hunt.png'
-import rupture from '../../Assets/rupture.jpg'
-import strokes from '../../Assets/strokes.jpg'
-import Toolstack from './Toolstack'
-import AwesomeSlider from 'react-awesome-slider';
-import 'react-awesome-slider/dist/styles.css';
+import { Container } from 'react-bootstrap'
+
+
+
+
 
 function About() {
   return (
     <Container fluid className="about-section">
       <Container>
-        <Row style={{ justifyContent: 'center', padding: '10px' }}>
-          <Col
-            md={5}
-            style={{ paddingTop: '50px', paddingBottom: '50px' }}
-            className="about-img"
-          >
-            <AwesomeSlider transitionDelay={0} className="img-fluid img-thumbnail rounded">
-              <div data-src={me4}  />
-              <div data-src={game} />
-              <div data-src={rupture} />
-              <div data-src={strokes} />
-            </AwesomeSlider>
-          </Col>
-          <Col
-            md={7}
-            style={{
-              justifyContent: 'center',
-              paddingTop: '30px',
-              paddingBottom: '50px'
-            }}
-          >
-            <h1 style={{ fontSize: '2.1em', paddingBottom: '20px' }}>
-              <strong className="light-blue">Conheça-me </strong>um pouco
-              mais...
-            </h1>
-            <Aboutcard />
-          </Col>
-        </Row>
-        <h1 className="project-heading">
-          Habilidades <strong className="light-blue">Profissionais</strong>
-        </h1>
-
-        <Techstack />
-
-        <h1 className="project-heading">
-          <strong className="light-blue">Programas</strong> que domino
-        </h1>
-        <Toolstack />
-
-        <Github />
-      </Container>
+     
+    </Container>
     </Container>
   )
 }
--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -2,26 +2,56 @@
 /* https://reactjsexample.com/a-lightweight-production-ready-carousel-for-react/ */
 
 import React from 'react'
-import { Container, Row, Col,Button } from 'react-bootstrap'
-   
-
-import a from "../../Assets/demo.png"
-
-
+import { Container, Row, Col, Button } from 'react-bootstrap'
+import gustavo from '../../Assets/Gustavo.png'
 
 function aboutHome() {
   return (
     <section>
-      <Container className='background1' fluid>
+      <Container className="background2" fluid>
         <Container>
           <Col md={12} className="d-flex justify-content-center mr-5 pt-5">
-            <h3 className=" heading1">Nossas</h3>
+            <h3 className=" heading1">Sobre</h3>
           </Col>
 
-          <h1 className="d-flex justify-content-center mr-5">Especialidades</h1>
-      
+          <h1 className="d-flex justify-content-center mr-5">Quem Somos</h1>
+        </Container>
+        <Container fluid>
           <Row>
-          <Button className='w-25 mx-auto my-4' variant="secondary">Ver todas</Button>{' '}
+            <Col md={6} className="p-2 mx-auto">
+              <h3 className="">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sit
+                amet velit vel elit posuere aliquam quis vitae sapien.{' '}
+              </h3>
+              <h4 className="">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sit
+                amet velit vel elit posuere aliquam quis vitae sapien. Maecenas
+                semper mollis sapien, sed posuere lectus commodo ac.{' '}
+              </h4>
+              <h4 className="">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sit
+                amet velit vel elit posuere aliquam quis vitae sapien. Maecenas
+                semper mollis sapien, sed posuere lectus commodo ac.{' '}
+              </h4>
+              <h4 className="">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sit
+                amet velit vel elit posuere aliquam quis vitae sapien. Maecenas
+                semper mollis sapien, sed posuere lectus commodo ac.{' '}
+              </h4>
+              <Button className="w-25 mx-auto my-4" variant="secondary">
+                Saiba mais
+              </Button>{' '}
+            </Col>
+            <Col md={6} className="d-flex justify-content-center">
+              <img
+                src={gustavo}
+                alt="home pic"
+                style={{
+                  
+                }}
+                className="thumbnail rounded mx-auto mt-4 h-75"
+              />
+            </Col>
           </Row>
         </Container>
       </Container>
